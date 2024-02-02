77f99f637f551b7d91e30f68b8de9d0de9fd4785
isromar <kaluca06@gmail.com>
2024-02-02 11:35:47 +0100

    Mejorada la explicación

--- traducciones/index.php
+++ traducciones/index.php
@@ -1,10 +1,10 @@
 <!--
-Este archivo surge porque al leer textos en inglés y aparecer algunas palabras que desconozco, pienso que estaría bien poder disponer de un diccionario propio donde poder almacenarlas y de ese modo facilitar la memorización.
-Funciona con un formulario que permite ingresar pares de frases en inglés y español. Almacena la información en una base de datos y muestra dinámicamente los registros ordenados alfabéticamente, además permite borrar los ya existentes.
+Esta web funciona con un formulario que permite ingresar pares de frases en inglés y español. Almacena la información en una base de datos y muestra dinámicamente los registros ordenados alfabéticamente, además permite borrar los ya existentes.
 
 @Author Isabel Rodenas Marin
 
 Para que funcione correctamente hay que crear la base de datos
+
 CREATE DATABASE IF NOT EXISTS traducciones;
 CREATE TABLE english_spanish (
     id INT AUTO_INCREMENT PRIMARY KEY,
