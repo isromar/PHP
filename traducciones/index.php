c6f27d9783cd7aa6c1b9ee8bbef9fe49b632f376
isromar <kaluca06@gmail.com>
2024-02-12 13:47:20 +0100

    Añadida columna de pronunciación a la tabla

--- traducciones/index.php
+++ traducciones/index.php
@@ -9,7 +9,8 @@ CREATE DATABASE IF NOT EXISTS traducciones;
 CREATE TABLE english_spanish (
     id INT AUTO_INCREMENT PRIMARY KEY,
     english_text VARCHAR(255) NOT NULL,
-    spanish_text VARCHAR(255) NOT NULL
+    spanish_text VARCHAR(255) NOT NULL,
+    pronunciation VARCHAR(255)
 );
 -->
 
@@ -28,7 +29,7 @@ CREATE TABLE english_spanish (
     }
 
     input[type="text"] {
-        width: 30%;
+        width: 20%;
         padding: 12px;
         border: 1px solid #ccc;
         border-radius: 4px;
@@ -37,6 +38,10 @@ CREATE TABLE english_spanish (
         text-align: center;
         /* Centrar el texto dentro del input */
         font-size: 16px;
+    }
+    
+    input#pronunciation {
+        width: 10%;
     }
 
     input[type="submit"] {
@@ -87,15 +92,16 @@ CREATE TABLE english_spanish (
 
 
 <body>
-
     <h2>Traductor</h2>
-
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="english">Inglés:</label>
         <input type="text" id="english" name="english">
 
         <label for="spanish">Español:</label>
         <input type="text" id="spanish" name="spanish">
+
+        <label for="pronunciation">Pronunciación:</label>
+        <input type="text" id="pronunciation" name="pronunciation">
 
         <input type="submit" name="submit" value="Guardar">
         <button id="refresh">Recargar</button>
@@ -111,21 +117,24 @@ CREATE TABLE english_spanish (
     }
 
     // Obtener las traducciones de la base de datos
-    $resultado = $conexion->query("SELECT id, english_text, spanish_text FROM english_spanish ORDER BY english_text");
+    $resultado = $conexion->query("SELECT id, english_text, spanish_text, pronunciation FROM english_spanish ORDER BY english_text");
 
     // Mostrar las traducciones en el formulario
     if ($resultado->num_rows > 0) {
         echo "<table>";
-        echo "<tr><th>Nº</th><th>Inglés</th><th>Español</th><th>Acción</th></tr>";
+        echo "<tr><th>Nº</th><th>Inglés</th><th>Español</th><th>Pronunciación</th><th>Acción</th></tr>";
         $contador = 1;  // Contador para numerar las filas de la tabla
         while ($row = $resultado->fetch_assoc()) {
-            echo "<tr><td>$contador</td><td>" . $row["english_text"] . "</td><td>" . $row["spanish_text"] . "</td>";
+            echo "<tr><td>$contador</td><td>" . $row["english_text"] . "</td><td>" . $row["spanish_text"] . "</td><td>" . $row["pronunciation"] . "</td>";
             echo "<td><form method='post' action='" . $_SERVER['PHP_SELF'] . "'><input type='hidden' name='id' value='" . $row["id"] . "'><input type='submit' name='delete' value='Borrar'></form></tr>";
             $contador++;
         }
         echo "</table>";
     } else {
         echo "0 resultados";
+
+    // Recargar la página
+    echo "<meta http-equiv='refresh' content='0'>";
     }
 
     // Procesar la eliminación cuando se envíe
@@ -153,15 +162,15 @@ CREATE TABLE english_spanish (
         $conexion->close();
 
         // Recargar la página
-        //echo "<meta http-equiv='refresh' content='0'>";
+        echo "<meta http-equiv='refresh' content='0'>";
     }
     ?>
 
     <?php
     // Procesar el formulario cuando se envíe
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Verificar que ambos campos tengan texto
-        if (!empty($_POST['spanish']) && !empty($_POST['english'])) {
+        // Verificar que todos los campos tengan texto
+        if (!empty($_POST['spanish']) && !empty($_POST['english']) && !empty($_POST['pronunciation'])) {
             // Conectar a la base de datos
             $conexion = new mysqli("localhost", "root", "", "traducciones");
 
@@ -173,10 +182,10 @@ CREATE TABLE english_spanish (
             // Obtener los valores del formulario y eliminar espacios al inicio y final
             $spanish = $conexion->real_escape_string(trim($_POST['spanish']));
             $english = $conexion->real_escape_string(trim($_POST['english']));
-
+            $pronunciation = $conexion->real_escape_string(trim($_POST['pronunciation']));
 
             // Insertar los valores en la base de datos
-            $sql = "INSERT INTO english_spanish (english_text, spanish_text) VALUES ('$english', '$spanish')";
+            $sql = "INSERT INTO english_spanish (english_text, spanish_text, pronunciation) VALUES ('$english', '$spanish', '$pronunciation')";
             if ($conexion->query($sql) === TRUE) {
                 echo "Traducción guardada correctamente";
             } else {
@@ -186,10 +195,9 @@ CREATE TABLE english_spanish (
             // Cerrar la conexión a la base de datos
             $conexion->close();
         } else {
-            echo "Ambos campos deben tener texto";
+            echo "Todos los campos deben tener texto";
         }
-
-    }
+}
     ?>
 
 </body>
